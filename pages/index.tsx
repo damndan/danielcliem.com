27266659350caa6a179c2d85237e07b8164faf20
Daniel Liem <daniel@pesto.app>
2024-03-10 20:22:48 -0700

    latest updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,19 +20,20 @@ export default function Home() {
 
           hey, world. i&apos;m <u><a href="https://www.linkedin.com/in/dcliem/" target="_blank">Daniel</a></u>. i&apos;m <u><a href="https://twitter.com/damndanielliem/status/1536403331714756609" target="_blank">happily married</a></u>, 
           a tech enthusiast, an active investor, 
-          an <u><a href="https://strava.com/athletes/damndan" target="_blank">addicted athlete</a></u> & 
-          dog lover (<u><a href="https://www.instagram.com/damndanielliem/" target="_blank">my favicon</a></u>). i helplessly build things as a hobby.
+          an <u><a href="https://strava.com/athletes/damndan" target="_blank">addicted athlete</a></u> &&nbsp;
+          <u><a href="https://twitter.com/damndanielliem/status/1624888220180951043?s=46&t=tI02mzs0UEAg-jdhLJ5cEA" target="_blank">dog lover</a></u>. i helplessly build things as a hobby.
         </div>
         <br />
         <ul className={styles.ul}>
           <li>
-            co-founding <u><a href="https://gentrace.ai/" target="_blank">Gentrace.ai</a></u> – an eval / observability tool for LLMs,
-            with <u><a href="https://www.linkedin.com/in/dsafreno/" target="_blank">Doug</a></u> & <u><a href="https://www.linkedin.com/in/vnair611/" target="_blank">Vivek</a></u>. we've $3.2M seed; we believe AI is going to change everything
+            co-founder, COO @ <u><a href="https://gentrace.ai/" target="_blank">Gentrace</a></u> – test and prod evaluation infrastructure for gen AI,
+            with <u><a href="https://www.linkedin.com/in/dsafreno/" target="_blank">Doug</a></u> & <u><a href="https://www.linkedin.com/in/vnair611/" target="_blank">Vivek</a></u>. customers include several growth/public tech cos; we've $3.75M seed, 
+            advisors/backers include founders x leaders @ Figma, Webflow, Linear, Asana, Faire, Ironclad, Handshake, Instawork, Instabase, Carta, Chronosphere, +more. 
           </li>
           <br />
           <li>
-            early stage investing in future of work and SaaS: productivity tools, infra / devOps, AI, vertical SaaS.
-            both personally and as a scout @ <u><a href="https://www.craftventures.com/" target="_blank">Craft ventures</a></u> (David Sack&apos;s fund; $3B+ AUM):
+            startup investing in B2B x SaaS: productivity tools, future of work, infra / devOps, AI, vertical SaaS.
+            both personally x scout @ <u><a href="https://www.craftventures.com/" target="_blank">Craft ventures</a></u> (Sack&apos;s fund; $3B+ AUM):
           </li>
           <br />
 
@@ -40,11 +41,13 @@ export default function Home() {
           <br />
           • <u><a href="https://n8n.io/" target="_blank">n8n.io</a></u> – pre-seed (personal)
           <br />
-          • <u><a href="https://www.playbook.com/" target="_blank">Playbook.com</a></u> – (personal)
+          • <u><a href="https://gitar.co/" target="_blank">Gitar.co</a></u> – seed (personal)
+          <br />
+          • <u><a href="https://www.playbook.com/" target="_blank">Playbook.com</a></u> – personal
           <br />
           • <u><a href="https://sironamedical.com/" target="_blank">Sirona Medical</a></u> – pre-seed (personal)
           <br />
-          • <u><a href="https://launchnotes.com/" target="_blank">Launchnotes.com</a></u> – (craft)
+          • <u><a href="https://launchnotes.com/" target="_blank">Launchnotes.com</a></u> – craft
           <br />
           • <u><a href="https://oslash.com/" target="_blank">Oslash.com</a></u> – seed (personal)
           <br />
@@ -54,21 +57,25 @@ export default function Home() {
           <br />
           • <u><a href="https://www.askviable.com/" target="_blank">Viable.fit</a></u> – seed (personal)
           <br />
-          • <u><a href="https://headsup.ai/" target="_blank">Headsup.ai</a></u> – seed (personal)
+          • <u><a href="https://headsup.ai/" target="_blank">Headsup.ai</a></u> (acq by Hightouch) – seed (personal)
           <br />
-          • <u><a href="https://cooby.co/" target="_blank">Cooby.co</a></u> – (craft)
+          • <u><a href="https://cooby.co/" target="_blank">Cooby.co</a></u> – craft
+          <br />
+          • Pantheon – seed (craft)
           <br />
           • <u><a href="https://highlight.run/" target="_blank">Highlight.run</a></u> – seed (personal)
-
-          <br /><br />
-
-          others not particularly SaaS but i&apos;ve worked with / believe in the founder:
-
-          <br /><br />
-
-          • <u><a href="https://www.nitra.com/" target="_blank">Nitra.com</a></u> – seed (craft)
           <br />
-          • <u><a href="https://livekindred.com/" target="_blank">Kindred</a></u> – (personal)
+          • <u><a href="https://www.jerichosecurity.com/" target="_blank">Jericho Security</a></u> – seed (craft)
+          <br />
+          • Codia – pre-seed (craft)
+
+          <br /><br />
+
+          others not particularly B2B but has angle in future of work x vertical SaaS / i&apos;ve worked with / believe in the founder:
+
+          <br /><br />
+
+          • <u><a href="https://livekindred.com/" target="_blank">Kindred</a></u> – personal
           <br />
           • <u><a href="https://www.melioratherapeutics.com/" target="_blank">Meliora Therapeutics</a></u> – seed (personal)
           <br />
@@ -76,33 +83,35 @@ export default function Home() {
           <br />
           • <u><a href="https://www.raincards.xyz/" target="_blank">Rain.xyz</a></u> – seed (personal)
           <br />
+          • <u><a href="https://www.nitra.com/" target="_blank">Nitra.com</a></u> – seed (craft)
+          <br />
 
           <br />
           <li>
             prev did my tour around SV:
-            investor @ Obvious ventures (Ev William&apos;s fund; $1B+ AUM), product @ Uber (platform), Dropbox & FiscalNote (all three of which have since gone public),
-            specializing in infra / devOps, mobile & SaaS products
+            investor @ obvious (Ev William&apos;s fund; $1B+ AUM), PM @ Uber (platform), Dropbox (mobile platform) & FiscalNote (all three of which have since gone public),
+            specializing in test, release & CI/CD
           </li>
           <br />
           <li>
-            graduated CS @ Stanford; dropped out of Masters in BMI
+            graduated CS (HCI) @ Stanford; dropped out of BMI masters
           </li>
           <br />
           <li>
-            own a home in Sunset, SF for 5+ yrs, Bay Area 12+; Arcadia, LA native (went to AHS) w one slightly smarter brother. Thought I was going to be a doctor
+            own a home in Sunset, SF for 6+ yrs, Bay Area 13+; Arcadia, LA native (went to AHS) w one slightly smarter brother. Thought I was going to be a doctor
           </li>
           <br />
           <li>
             addicted to cycling;
-            own an Ultra Stealth <u><a href="https://www.canyon.com/en-us/road-bikes/aero-bikes/aeroad/cfr/" target="_blank">Canyon Aeroad CFR disc Di2</a></u> (wedding gift)
+            own an Ultra Stealth <u><a href="https://www.canyon.com/en-us/road-bikes/aero-bikes/aeroad/cfr/aeroad-cfr-etap/3950.html?dwvar_3950_pv_rahmenfarbe=R065_P12" target="_blank">Canyon Aeroad CFR disc Di2</a></u>
           </li>
           <br />
           <li>
-            co-own (w in-laws) a 7yr 90lb lab named <u><a href="https://twitter.com/damndanielliem/status/1624888220180951043?s=46&t=tI02mzs0UEAg-jdhLJ5cEA" target="_blank">Max</a></u>
+            have 8yr 90lb lab named <u><a href="https://twitter.com/damndanielliem/status/1624888220180951043?s=46&t=tI02mzs0UEAg-jdhLJ5cEA" target="_blank">Max</a></u>
           </li>
           <br />
           <li>
-            occassionally on <u><a href="https://twitter.com/damndanielliem" target="_blank">Twitter</a></u>, even though it&apos;s largely a distraction..
+            occassionally on <u><a href="https://twitter.com/damndanielliem" target="_blank">X</a></u>, even though it&apos;s largely a distraction..
           </li>
           <br />
           <li>
